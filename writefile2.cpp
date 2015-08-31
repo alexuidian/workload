e8616323f3f55087566fde372b4bbed83235fb86
Shubham <shubhamiit@xlr.(none)>
2015-09-01 02:16:25 +0530

    Yes

--- writefile2.cpp
+++ writefile2.cpp
@@ -14,6 +14,9 @@
 #include <stdio.h>
 
 unsigned int random_seed=0;
+
+#define get_random() (11*rand()^(rand() << 15)^((long long int)rand()<< 30))
+
 #if defined(_WIN32)
 #include <Windows.h>
 
@@ -109,10 +112,172 @@ double getRealTime( )
 
 using namespace std;
 
+
+/* RANDOM GENERATOR xlr */
+
+static unsigned long long int varx=123456789,vary=362436000,varz=521288629,varc=7654321;
+
+void xlr_srand(unsigned long long x)
+{
+
+    varx= ( x%17 * (x<<13) ) + x*x + 69069 * x + 12345;
+    vary= (x*2109210)%107 + 2121212 * x+1;
+    varz= ((x*x*x)>>3) + (x%7 * (x <<6)) + 101112*(x^13);
+    varc= (x+2*x+3*x)*(x%3)+(12121)*x+7;
+}
+
+unsigned int xlr_rand()
+{
+    unsigned long long t, a=698769069ULL;
+    varx=69069*varx+12345;
+    vary^=(vary<<13); vary^=(vary>>17); vary^=(vary<<5);
+    t=a*varz+varc;
+    varc=(t>>32);
+    return varx+vary+(varz=t);
+}
+
+
+
+/* xlr Random generator finish*/
+
+/* xlr create file start */
+
+unsigned long long global_total_blocks=0;
+unsigned long long global_unique_blocks=0;
+unsigned long long global_remaining_unique_blocks=0;
+unsigned long long global_remaining_total_blocks=0;
+unsigned long long global_remaining_nonunique_blocks=0;
+int entry_create_filesystem=0;
+unsigned long long prob1=100;
+unsigned long long prob2=100;
+unsigned int comp=0,dedup=0,tze_save=0;
+
+
+int compression_bucket[]={30,40,50,60,70,80,85,90,95,98,100};
+static char *unique_checker=NULL;
+unsigned int x_debug=0;
+
+int fetch_from_bucket(int comp)
+{
+    int bucket=comp/10;
+    int mod=comp%10;
+    if(comp==0) return 0;
+    if(bucket>=10)
+        return 100;
+    if(bucket == 9)
+    {
+        return 98+(2*mod)/10;
+    }
+    return compression_bucket[bucket]+ (compression_bucket[bucket+1] - compression_bucket[bucket])*mod/10;
+
+}
+
+unsigned long long find_good_block()
+{
+    //int good_block=0;
+    unsigned long long prob3;
+   unsigned long int temprandom;
+    int randomx,randomy;
+
+    prob3= ( 100 * (global_total_blocks - global_unique_blocks))/(global_unique_blocks);
+    global_remaining_total_blocks--; 
+    
+    if(global_remaining_nonunique_blocks <= 0 && global_remaining_unique_blocks > 0 && global_remaining_total_blocks > 0 )
+    {
+            global_remaining_unique_blocks--; 
+            if(unique_checker != NULL)
+            {
+                for(temprandom= get_random()%global_unique_blocks;unique_checker[temprandom]==0 ;temprandom=get_random()%global_unique_blocks);
+                    unique_checker[temprandom]=0;   
+                    return temprandom;                
+            }
+            else
+                printf("ERROR OCCURED\n");
+    }
+
+    if(global_remaining_unique_blocks > 0 && global_remaining_total_blocks >0 && global_remaining_nonunique_blocks > 0)
+    {
+        prob2= ( 100 * global_remaining_nonunique_blocks)/ global_remaining_unique_blocks;
+    
+        if( (prob2 + 2) < prob3)
+            prob1+=5;
+        
+        else if( (prob2 - 5) > prob3)
+            prob1-=2;
+        
+        randomx= ((int)get_random() % 100);
+        randomy=(int) prob1;
+        if(randomx < randomy)
+        {
+            global_remaining_unique_blocks--;
+            if(unique_checker != NULL)
+            {
+                for(temprandom= get_random()%global_unique_blocks;unique_checker[temprandom]==0 ;temprandom=get_random()%global_unique_blocks);
+                    unique_checker[temprandom]=0;   
+                    return temprandom;
+                
+            }
+            else
+                printf("ERROR OCCURED\n");
+        
+
+        }
+        else
+        {
+                global_remaining_nonunique_blocks--;
+                return get_random() % global_unique_blocks;
+        }
+        
+    
+
+    }
+    else
+    {
+        if(global_remaining_nonunique_blocks <=0 || global_remaining_unique_blocks <=0 || global_remaining_total_blocks <=0 )
+            return (int)(get_random()%global_unique_blocks); 
+    }
+    
+      return 0;
+}
+
+void fillbuffer2(char *buf, unsigned long long  blocksize)
+{ 
+    unsigned long long which_block=0;
+    int r;
+    unsigned long long i=0;
+
+    which_block=find_good_block();    
+
+    xlr_srand(which_block);   
+    //printf("RANDOM SEED: %d\n",which_block);
+    for(i=0;i<blocksize;i++)
+    {
+        r=xlr_rand() + xlr_rand();
+        buf[i]=(r)%26+'a';
+        if((xlr_rand()%101) <= fetch_from_bucket(comp))
+            buf[i]='0';
+    }
+   
+   //cout << blocksize << endl; 
+   buf[blocksize-1]='\n';
+
+}
+
+
+
+
+
+
+
+
+
+
 void fillbuffer(char *buffer, unsigned long long ts)
 {
 	time_t t;
 unsigned long long i;
+
+	printf("%llu\n",ts);
 //srand(random_seed);
 	for(i=0;i<ts-1;i++)
 	{
@@ -120,8 +285,7 @@ unsigned long long i;
 	
 		buffer[i]=(r)%26+'a';
 	}
-	buffer[i]='\0';
-
+	buffer[i]='\n';
 }
 
 int main(int argc, char *argv[])
@@ -131,7 +295,7 @@ if(argc < 2)
 {
 
 	cout <<  
-	"Usage: " << argv[0] << " Filename filesize_in_GB  r/w/b/n/o  [buffersize] runtime  randomseed\n " <<
+	"Usage: " << argv[0] << " Filename filesize_in_GB  r/w/b/n/o  [buffersize] runtime  randomseed [dedup] [com_sav] [tze_save]\n " <<
 	"r: random_read\n w: random_write \n n: pure_random_read \n o: directio_random_read\n runtime is in seconds \n buffer size is in KB \n Filesize in GB \n all parametrs are compulsory and parameters which are not going to be used can be passwd with any value \n"
 	;
 	return -1;
@@ -141,7 +305,7 @@ if(argc < 2)
 if(!strcmp(argv[1],"--help"))
 {
 	cout <<  
-	"Usage: " << argv[0] << " Filename filesize_in_GB  r/w/b/n/o  [buffersize] runtime randomseed \n " <<
+	"Usage: " << argv[0] << " Filename filesize_in_GB  r/w/b/n/o  [buffersize] runtime randomseed  [dedup] [com_sav] [tze_save] \n " <<
 	"r: random_read\n w: random_write \n n: pure_random_read \n o: directio_random_read\n m: modify \n runtime is in seconds \n buffer size is in KB \n Filesize in GB \n all parametrs are compulsory and parameters which are not going to be used can be passwd with any value \n"
 	;
 	return -2;
@@ -149,7 +313,7 @@ if(!strcmp(argv[1],"--help"))
 
 	if(argc < 7)
 	{
-	cout << "Usage: " << argv[0] << " Filename filesize_in_GB  r/w/b/n/o/m  [buffersize] runtime  randomseed\n " << argv[0] << "--help " << endl;
+	cout << "Usage: " << argv[0] << " Filename filesize_in_GB  r/w/b/n/o/m  [buffersize] runtime  randomseed [dedup] [com_sav] [tze_save]\n " << argv[0] << "--help " << endl;
 	return -1;
 	}
 
@@ -208,15 +372,24 @@ else buffsize=4;
 random_seed=atoi(argv[6]);
 srand(random_seed);
 
+if(argc >= 8)
+	dedup=atoi(argv[7]);
+if(argc >= 9)
+	comp=atoi(argv[8]);
+if(argc >=10)
+	tze_save=atoi(argv[9]);
 
-unsigned long long tb=buffsize*1000;
+
+
+
+
+unsigned long long tb=buffsize*1024;
 char *buffer=(char *) malloc(tb);
-fillbuffer(buffer, tb);
 
-	double seq_perf2=0,rand_perf2=0,xperf2=0,actualtime2=0;
-	unsigned long long int tcc2=0, to2=0;
-		double seq_perf1=0,rand_perf1=0,xperf1=0,actualtime1=0;
-	unsigned long long int tcc1=0, to1=0;
+double seq_perf2=0,rand_perf2=0,xperf2=0,actualtime2=0;
+unsigned long long int tcc2=0, to2=0;
+double seq_perf1=0,rand_perf1=0,xperf1=0,actualtime1=0;
+unsigned long long int tcc1=0, to1=0;
 
 
 
@@ -232,18 +405,43 @@ if((mode == 1) || (mode == 2))
 	double endt=getRealTime();
 	unsigned long long int total_cpu_cycles=0, total_operations=0;
 
+	unsigned long long unique_blocks=0;
+    unsigned long long total_blocks ;
+
+
 	if(myfile.is_open())
 	{
-		fillbuffer(buffer,tb);
-		unsigned long int totals=totalsize*1000*1000;
+		
+		unsigned long int totals=totalsize*1024*1024;  // converting into KB
 		unsigned long long counts=totals/buffsize;
 		int rem= totals% buffsize;
+
+		total_blocks = totals / buffsize;
+		if(total_blocks <1)
+            total_blocks=1;
+
+        if(dedup > 99)
+            dedup=99;
+
+        unique_blocks = (total_blocks * (100 - dedup )) / 100;
+        if(unique_blocks <1) unique_blocks=1;
+         global_remaining_unique_blocks=unique_blocks;
+         global_unique_blocks=unique_blocks;
+         global_total_blocks=total_blocks;
+         global_remaining_total_blocks=total_blocks;
+         global_remaining_nonunique_blocks= global_total_blocks - global_unique_blocks;
+         unique_checker=(char *)malloc(sizeof(char) * global_unique_blocks);
+         for(int i=0;i<global_unique_blocks;i++)
+                unique_checker[i]=1;
+         prob1= ((global_unique_blocks) * 100) / global_total_blocks;
+         prob2= ((global_total_blocks- global_unique_blocks) * 100) / global_unique_blocks;
+
 		//cout << totals << " KB  " << buffsize << " KB  "  << counts << endl ;
 		
 		time_t starttime=clock();
 		while(counts--)
 		{
-			fillbuffer(buffer,tb);
+			fillbuffer2(buffer,tb);
 			starttime=clock();
 			myfile.write(buffer,tb);
 			end_time=clock();
@@ -271,7 +469,7 @@ if((mode == 1) || (mode == 2))
 	 while(timesec > (getRealTime() - random_start) )
 	  {
 		
-		unsigned long long MOD= totals - buffsize;
+		unsigned long long MOD= totals - tb;
 		if(MOD < 1) break;
 		
 		time_t t;
@@ -280,7 +478,7 @@ if((mode == 1) || (mode == 2))
 		
 		starttime=clock();
 		fillbuffer(buffer,tb);
-		myfile.seekp(randomint*1000,ios::beg);
+		myfile.seekp(randomint*1024,ios::beg);
 		myfile.write (buffer, tb);
 		end_time=clock();
 		total_cpu_cycles+=end_time - starttime ;
@@ -303,9 +501,9 @@ if((mode == 1) || (mode == 2))
 		double ran_performance=(double)(random_ops*buffsize);
 		double xperformance=(double) (total_operations*buffsize);
 		//cout << " Actual time is  " << (endt-startt) << endl ;
-		seq_performance= seq_performance / ((random_start - startt )*1000);
-		ran_performance= ran_performance/ ((endt - random_start)*1000);
-		xperformance= xperformance /((endt - startt)*1000);
+		seq_performance= seq_performance / ((random_start - startt )*1024);
+		ran_performance= ran_performance/ ((endt - random_start)*1024);
+		xperformance= xperformance /((endt - startt)*1024);
 		//cout << "CPU time is " << total_cpu_cycles << "  and total operations are " << total_operations << endl;
 		//cout << "Performance for writing for file of  " <<  totals << " MB" << " is " << performance << " MBPS" << endl ;
 		if(mode !=2)
@@ -324,6 +522,7 @@ if((mode == 1) || (mode == 2))
 		actualtime1=(endt-startt);
 		tcc1=total_cpu_cycles;
 		to1=total_operations;
+		free(unique_checker);
 	}
 	else cout << "Unable to open file\n";	
 }
@@ -343,7 +542,7 @@ if((mode==0) || (mode ==2))
 	{
 	unsigned long long size= myfile.tellg();
 	cout<< "filesize: " << size  << endl ;
-	unsigned long long size_in_mb = size / 1000;
+	unsigned long long size_in_mb = size / 1024;
 	
 	unsigned long long counts = size_in_mb/buffsize;
 	
@@ -354,7 +553,7 @@ if((mode==0) || (mode ==2))
 	//cout << counts  << "  " << rem << endl;
 	while(counts--)
 	{
-		//fillbuffer(buffer, tb);
+		
 		starttime=clock();
 		myfile.read (buffer, tb); 	
 		end_time=clock();
@@ -386,14 +585,14 @@ if((mode==0) || (mode ==2))
 		unsigned long long int randomint= (my_big_random % MOD );
 		
 		starttime=clock();
-		myfile.seekg(randomint*1000,ios::beg);
+		myfile.seekg(randomint*1024,ios::beg);
 		myfile.read (buffer, tb);
 		end_time=clock();
 		total_cpu_cycles+=end_time - starttime ;
 		total_operations++;
 		random_ops++;
 	    //cout << buffer << endl; 	
-		//fillbuffer(buffer, tb);
+		
 		if(timesec <= (getRealTime() - random_start) ) break;		
 		//cout << myfile.tellg() << "Location  " << endl  ;
 	}
@@ -407,9 +606,9 @@ if((mode==0) || (mode ==2))
 		double ran_performance=(double)(random_ops*buffsize);
 		double xperformance=(double) (total_operations*buffsize);
 		//cout << " Actual time is  " << (endt-startt) << endl ;
-		seq_performance= seq_performance / ((random_start - startt )*1000);
-		ran_performance= ran_performance/ ((endt - random_start)*1000);
-		xperformance= xperformance /((endt - startt)*1000);
+		seq_performance= seq_performance / ((random_start - startt )*1024);
+		ran_performance= ran_performance/ ((endt - random_start)*1024);
+		xperformance= xperformance /((endt - startt)*1024);
 		//cout << "CPU time is " << total_cpu_cycles << "  and total operations are " << total_operations << endl;
 		//cout << "Performance for writing for file of  " <<  totals << " MB" << " is " << performance << " MBPS" << endl ;
 		if(mode !=2)
@@ -472,7 +671,7 @@ if(mode==4)
 
 	//char *block=(char *)buffer;
 	//bufferx=(void *)malloc(blocksize);
-	//fillbuffer(block, blocksize);
+	
 	unsigned long long int counts=filesize_in_B/blocksize;
 	int rem=filesize_in_B%blocksize;
 	
@@ -484,7 +683,7 @@ if(mode==4)
 		total_cpu_cycles+=end_time - starttime ;
 		total_operations++;
 	//	cout << bufferx;
-		//fillbuffer(buffer, tb);		
+				
 		//cout << myfile.tellg() << "Location  " << endl  ;
 	}
 	if(rem)
@@ -524,7 +723,7 @@ if(mode==4)
 		total_operations++;
 		random_ops++;
 	    //cout << buffer << endl; 	
-		//fillbuffer(buffer, tb);
+		
 	//	if(timesec <= (getRealTime() - random_start) ) break;		
 		//cout << myfile.tellg() << "Location  " << endl  ;
 	}
@@ -535,7 +734,7 @@ if(mode==4)
 	close(f);
 	endt=getRealTime();
 
-	int filesize_inMB=(filesize_in_B/1000);
+	int filesize_inMB=(filesize_in_B/1024);
 	
 	
 		double seq_performance=(double)(filesize_inMB);
@@ -543,9 +742,9 @@ if(mode==4)
                 double xperformance=(double) (total_operations*blocksize);
 		cout << "total ops: " << total_operations << endl;
 		cout << "total time: " << endt - startt << endl;
-                seq_performance= seq_performance / ((random_start - startt )*1000);
-                ran_performance= ran_performance/ ((endt - random_start)*1000);
-                xperformance= xperformance /((endt - startt)*1000);
+                seq_performance= seq_performance / ((random_start - startt )*1024);
+                ran_performance= ran_performance/ ((endt - random_start)*1024);
+                xperformance= xperformance /((endt - startt)*1024);
          cout << "#seq_perf rand_perf #avf_perf #total_ops #cpucycles/ops #cpucycles\n";
        	 cout << seq_performance << " " << ran_performance << " " << xperformance << " " << total_operations << " " << total_cpu_cycles/total_operations << endl;
 
@@ -589,7 +788,7 @@ if(mode == 3)
 	{
 	unsigned long long size= myfile.tellg();
 	cout<< "#File size: " << size  << endl ;
-	unsigned long long size_in_mb = size / 1000;
+	unsigned long long size_in_mb = size / 1024;
 	
 	unsigned long long counts = size_in_mb/buffsize;
 	
@@ -622,7 +821,7 @@ if(mode == 3)
 		total_cpu_cycles+=end_time - starttime ;
 		total_operations++;
 	        //cout << buffer << endl; 	
-		//fillbuffer(buffer, tb);
+		
 		if(timesec <= (getRealTime() - startt) ) break;		
 		//cout << myfile.tellg() << "Location  " << endl  ;
 	}
@@ -670,9 +869,9 @@ if(mode==5)
 	
         if(myfile.is_open())
         {
-		tb=1000;
+		tb=1024;
                 fillbuffer(buffer,tb);
-                unsigned long int totals=totalsize*1000*1000;
+                unsigned long int totals=totalsize*1024*1024;
                 unsigned long long counts=totals/buffsize;
 		counts=counts/2;
 		
@@ -686,7 +885,7 @@ if(mode==5)
                 	unsigned long long int randomint= (my_big_random % MOD );			
 			//printf("Random ints are%llu \n", randomint);
                         fillbuffer(buffer,tb);
-			myfile.seekp(randomint*1000,ios::beg);
+			myfile.seekp(randomint*1024,ios::beg);
                         myfile.write(buffer,tb);
 
                 }
